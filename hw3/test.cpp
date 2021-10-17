106ae6bd8121ad015eadb59bd78e4317e941ec23
Keyu Zhang <keyuz4@uci.edu>
2021-10-16 19:48:58 -0700

    finish testing

--- hw3/test.cpp
+++ hw3/test.cpp
@@ -22,7 +22,78 @@ int main() // I’ll start it for you
 
     // add at least one test case for each method defined in Vector
 
+    // Vector(int n)
     Vector<int> a(10);
+    Vector<double> b(10);
+
+    // Vector(std::initializer_list)
+    Vector<int> c{1,2,3,4,5,6};
+    Vector<double> d{0.1, 0.2, 0.3, 0.4};
+    
+    // size()
+    std::cout << "size:" << std::endl;
+    std::cout << a.size() << std::endl;
+    std::cout << b.size() << std::endl;
+    std::cout << c.size() << std::endl;
+    std::cout << d.size() << std::endl;
+
+    // operator[]
+    std::cout << c[1] << " " << d[2] << std::endl;
+    try
+    {
+        int temp = a[20];
+    }
+    catch(const std::out_of_range& e)
+    {
+        std::cout << "catched" << std::endl;
+    }
+
+    // operator * 
+    Vector<int> v1{1,2};
+    Vector<int> v2{3,4,5};
+    std::cout << v1 * v2 << std::endl;
+    std::cout << v2 * v1 << std::endl;
+    Vector<int> v3{};
+    std::cout << v2 * v3 << std::endl;
+
+    // operator + 
+    std::cout << v1 + v2 << std::endl;
+    std::cout << v2 + v1 << std::endl;
+    
+    // operator =
+    v3 = v2;
+    std::cout << v2 << std::endl;
+    std::cout << v3 << std::endl;
+
+    // operator ==
+    if (v2 == v3)
+    {
+        std::cout << "v2 == v3" << std::endl;
+    }
+    else
+    {
+        std::cout << "v2 != v3" << std::endl;
+    }
+    // operator !=
+    if (v1 != v3)
+    {
+        std::cout << "v1 != v3" << std::endl;
+    }
+    else
+    {
+        std::cout << "v1 == v3" << std::endl;
+    }
+    
+    // operator * with int
+    Vector<double> v4{0.1, 0.2, 0.3, 0.4};
+    std::cout << 20 * v2 << std::endl;
+    std::cout << 20 * v4 << std::endl;
+
+    // operator + with int
+    std::cout << 20 + v2 << std::endl;
+    std::cout << 20 + v4 << std::endl;
+
+
 
 
     return 0;
--- hw3/vector.h
+++ hw3/vector.h
@@ -13,26 +13,22 @@ class Vector {
 private:
     int sz;     // the number of elements in this Vector
     T* buf;     // the base of the array of Ts, you must allocate it
-    unsigned int bufSz;
 public:
     Vector(int n) // Vector v1(10); -- create a 10 element Vector
     {
-        buf = new T[2*n+1];
-        sz = 0;
-        bufSz = 2*n+1;
+        buf = new T[n];
+        sz = n;
     }
     Vector(std::initializer_list<T> L) // Vector v1{T1, T2, T3};
     {
-        int size = L.size();
-        buf = new T[size * 2 + 1];
-        sz = size;
-        bufSz = 2 * size + 1;
+        buf = new T[L.size()];
         unsigned int i = 0;
         for (auto j : L)
         {
             buf[i] = j;
             i++;
         }
+        sz = L.size();
     }
     ~Vector() // destructor called automatically when a Vector dies 
     {
@@ -41,8 +37,7 @@ public:
 /* Destructor should free memory used. your program should have no leak/lost/still-reachable/errors(suppressed or not), besides 72704 bytes in one still-reachable block (a g++/valgrind bug on some versions). */
     Vector(const Vector & v) // Vector v2(v1); deep-copy
     {
-        buf = new T[v.bufSz];
-        bufSz = v.bufSz;
+        buf = new T[v.size()];
         for (unsigned int i = 0; i < v.size(); i++)
         {
             buf[i] = v.buf[i];
@@ -61,16 +56,16 @@ public:
         }
         else
         {
-            throw std::out_of_range{};
+            throw std::out_of_range{"out of range"};
         }
     }
 /*Access out-of-bound index should throw an error to be caught in outside scope */
     T operator * (const Vector & v) const
     {
         T result = 0;
-        for (unsigned int i = 0; i < std::min(this.size, v.size()); i++)
+        for (unsigned int i = 0; i < std::min(sz, v.size()); i++)
         {
-            result += buf[i] + v.buf[i];
+            result += buf[i] * v.buf[i];
         }
         
         return result;
@@ -81,61 +76,53 @@ public:
     Vector operator + (const Vector & v) const 
 // V3 = V1 + V2; [1, 2, 3] + [4, 5, 6, 7] = [5, 7, 9, 7]
     {
-        Vector<T> result(std::max(v.size(), this.size()));
-        for (unsigned int i = 0; i < std::min(v.size(), this.size()); i++)
+        Vector<T> result(std::max(v.size(), sz));
+        for (unsigned int i = 0; i < std::min(v.size(), sz); i++)
         {
-            result[i] = this[i] + v[i];
+            result[i] = buf[i] + v.buf[i];
         }
-        if (this.size() < v.size())
+        if (sz < v.size())
         {
             // this is shorter
-            for (unsigned int i = this.size(); i < v.size(); i++)
+            for (unsigned int i = sz; i < v.size(); i++)
             {
-                result[i] = v[i];
+                result[i] = v.buf[i];
             }
         }
         else
         {
             // v is shorter
-            for (unsigned int i = v.size(); i < this.size(); i++)
+            for (unsigned int i = v.size(); i < sz; i++)
             {
-                result[i] = this[i];
+                result[i] = buf[i];
             }
         }
         return result;
     }
     const Vector & operator = (const Vector & v) // V1 = V2;
     {
-        T* newBuf;
-        unsigned int newBufSz;
-        if (v.bufSz > bufSz)
+        if (this != &v)
         {
-            newBuf = new T[v.bufSz];
-            newBufSz = v.bufSz;
+            T* newBuf = new T[v.size()];
+            for (unsigned int i = 0; i < v.size(); i++)
+            {
+                newBuf[i] = v.buf[i];
+            }
+            delete buf;
+            buf = newBuf;
+            sz = v.size();
         }
-        else
-        {
-            newBuf = buf;
-            newBufSz = bufSz;
-        }
-        for (unsigned int i = 0; i < v.size(); i++)
-        {
-            newBuf[i] = v.buf[i];
-        }
-        delete buf;
-        buf = newBuf;
-        bufSz = newBufSz;
-        sz = v.size();
+        return *this;
     }
     bool operator == (const Vector & v) const // if (V1 == V2)...
     {
-        if (this.size() != v.size())
+        if (sz != v.size())
         {
             return false;
         }
-        for (unsigned int i = 0; i < this.size(); i++)
+        for (unsigned int i = 0; i < sz; i++)
         {
-            if (buf[i] != v[i])
+            if (buf[i] != v.buf[i])
             {
                 return false;
             }
@@ -144,7 +131,7 @@ public:
     }
     bool operator != (const Vector & v) const // if (V1 != V2)...
     {
-        return !(this == v);
+        return !(*this == v);
     }
     friend Vector operator * (const int n, const Vector & v)
     // V1 = 20 * V2; -- each element of V1 is element of V2 * 20
