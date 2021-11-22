7e318123900d37cb8048dbe0ffd6b1e6b0d82803
Keyu Zhang <keyuz4@uci.edu>
2021-11-22 00:16:46 -0800

    fix hw8

--- hw8/src/PrintJob.java
+++ hw8/src/PrintJob.java
@@ -23,10 +23,13 @@ class PrintJobThread
         int p = mainClass.pm.request();
         // for i in 0 to f.fileLength-1:
         for (int i = 0; i < f.fileLength-1; i++) {
-            mainClass.disks[d].read(start+i, line);
+            mainClass.disks[d].read(start+i, this.line);
             // note we can have many file readers, so no disk request required
-            mainClass.printers[p].print(line);
+            mainClass.printers[p].print(this.line);
+            System.out.println("printiiiiiiiiiii");
+            System.out.println(this.line.toString());
         }
         mainClass.pm.release(p);
+        System.out.println("print finish");
     }
 }
--- hw8/src/Printer.java
+++ hw8/src/Printer.java
@@ -5,30 +5,35 @@ import java.io.IOException;
 class Printer
     extends Thread
 {
-    FileWriter writer;
+    // FileWriter writer;
+    String filepath;
 
     Printer(int id)
     {
-        String filepath = String.format("PRINTER%d", id);
+        this.setDaemon(true);
+        this.filepath = String.format("PRINTER%d", id);
         try {
-            File f = new File(filepath);
+            File f = new File(this.filepath);
             f.createNewFile();
         } catch (IOException e) {
             System.out.println("File already exists");
         }
 
-        try {
-            this.writer = new FileWriter(filepath);
-        } catch (IOException e) {
-            //TODO: handle exception
-            e.printStackTrace();
-        }
+        // try {
+        //     this.writer = new FileWriter(filepath);
+        // } catch (IOException e) {
+        //     //TODO: handle exception
+        //     e.printStackTrace();
+        // }
     }
 
     void print(StringBuffer b)
     {
         try {
+            System.out.println("writing ");
+            FileWriter writer = new FileWriter(this.filepath);
             writer.write(b.toString());
+            writer.close();
             Thread.sleep(2750);
         } catch (IOException e) {
             // TODO Auto-generated catch block
